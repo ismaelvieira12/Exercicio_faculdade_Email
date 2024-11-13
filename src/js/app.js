e460bb684b7f35cf1fbe782ac4486220190369bc
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-13 11:36:05 -0300

    update

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('.btn');
+        
+        
+button.addEventListener('click', () => {
+    const form = document.querySelector('form');
+    const inputEmail = document.querySelector('#email').value;
+    //Expressão para validar um email;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    //verifica se o email é valido ou não.
+    if(regex.test(inputEmail)){
+        alert('O email digita é válido.');
+
+    }else{
+        alert("[ERRO!], O email digitado não é válido!");
+    }
+})
